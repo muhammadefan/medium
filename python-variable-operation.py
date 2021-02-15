11336bd72e448468d1c8e2d749f8285922b00834
Muhammad Efan Abdulfattah <efan.alfattah@gmail.com>
2021-02-15 21:19:23 +0700

    first commit
    python-variable-operation

---
+++ python-variable-operation.py
@@ -0,0 +1,69 @@
+### variable
+# single variable
+var = 1
+print(var)
+# multiple variable
+var,var2 = 'python',6
+print(var)
+print(var2)
+# variable swapping
+var,var2 = var2,var
+print(var)
+print(var2)
+
+
+### operation
+## arithmetic operation
+# addition
+data = 10+10
+print(data)
+
+print('Python '+'Mudah')
+# substraction
+data = 10 - 5
+print(data)
+# multiplication
+data = 10*2
+print(data)
+
+print('aku' * 3)
+# division
+data = 10/3
+print(data)
+# floor-division
+data = 10//3
+print(data)
+# modulo
+data = 10 % 3
+print(data)
+# power
+data = 10 ** 2
+print(data)
+
+## boolean/logical operation
+# equal
+print(10 == 5)
+# not equal
+print(10 != 5)
+# bigger than
+print(5 > 1)
+# less than
+print(5 < 1)
+# less than equal
+print(5 <= 1)
+# bigger than equal
+print(5 >= 1)
+
+## bitwise operation
+# AND
+print(7 & 3)
+print(7 and 3)
+# OR
+print(7 | 3)
+print(7 or 3)
+# XOR
+print(7 ^ 3)
+# right-shift
+print(7 >> 1)
+# left-shift
+print(7 << 1)
